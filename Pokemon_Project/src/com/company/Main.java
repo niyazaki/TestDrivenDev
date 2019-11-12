82d4e203bec87e71a855edc5742de990a7c4d2de
El Haddadi Yassine <36259631+16146@users.noreply.github.com>
2019-11-12 22:28:00 +0100

    Add Metrics

--- Pokemon_Project/src/com/company/Main.java
+++ Pokemon_Project/src/com/company/Main.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        ConcretePokemon pikachu = new PokemonBuilder("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
-        ConcretePokemon squirtle = new PokemonBuilder("Squirtle", new ConcreteType("Water"), new Stats(44,48,65,43));
+        //ConcretePokemon pikachu = new PokemonBuilder("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
+        //ConcretePokemon squirtle = new PokemonBuilder("Squirtle", new ConcreteType("Water"), new Stats(44,48,65,43));
 
-        Fight fight1 = new Fight(pikachu, squirtle);
+        //Fight fight1 = new Fight(pikachu, squirtle);
 
-        System.out.print(fight1.winner);
+        //System.out.print(fight1.winner);
     }
 
 }
--- Pokemon_Project/tests/com/company/FightTest.java
+++ Pokemon_Project/tests/com/company/FightTest.java
@@ -2,23 +2,34 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Timer;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FightTest {
 
     @Test
     void attack() {
-        ConcretePokemon Salamèche = new ConcretePokemon() ;
-        ConcretePokemon Bulbizar = new ConcretePokemon() ;
-        Fight.Attack(Salamèche, Bulbizar);
-        assertEquals(50, Salamèche.hp());
+        ConcretePokemon pikachu = new PokemonBuilder("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
+        ConcretePokemon squirtle = new PokemonBuilder("Squirtle", new ConcreteType("Water"), new Stats(50,48,65,43));
+        Fight.Attack(pikachu, squirtle);
+        assertEquals(50, pikachu.hp);
     }
 
     @Test
     void winner() {
-        ConcretePokemon Pikachu = new ConcretePokemon() ;
-        ConcretePokemon Bulbizar = new ConcretePokemon() ;
-        assertEquals("Pikachu", Fight.Winner(Pikachu, Bulbizar));
+        ConcretePokemon pikachu = new PokemonBuilder("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
+        ConcretePokemon squirtle = new PokemonBuilder("Squirtle", new ConcreteType("Water"), new Stats(10,48,65,43));
+        assertEquals("pikachu", Fight.Winner(pikachu, squirtle));
+
+    }
+    @Test
+    void metrics() {
+        long start = System.nanoTime();
+        //winner();
+        long finish = System.nanoTime();
+        long timeElapsed = finish - start;
+        System.out.print(timeElapsed);
 
     }
 }
