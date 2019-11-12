c22a35e4361d5f93dcb87dbd18d1a3bc14bcae4a
El Haddadi Yassine <36259631+16146@users.noreply.github.com>
2019-11-12 23:08:06 +0100

    Last Update Tests

--- Pokemon_Project/tests/com/company/FightTest.java
+++ Pokemon_Project/tests/com/company/FightTest.java
@@ -10,27 +10,27 @@ class FightTest {
 
     @Test
     void attack() {
-        ConcretePokemon pikachu = new PokemonBuilder("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
-        ConcretePokemon squirtle = new PokemonBuilder("Squirtle", new ConcreteType("Water"), new Stats(50,48,65,43));
+        ConcretePokemon pikachu = new ConcretePokemon("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
+        ConcretePokemon squirtle = new ConcretePokemon("Squirtle", new ConcreteType("Water"), new Stats(50,48,65,43));
         Fight.Attack(pikachu, squirtle);
-        assertEquals(50, pikachu.hp);
+        assertEquals(35, pikachu.hp);
     }
 
     @Test
     void winner() {
-        ConcretePokemon pikachu = new PokemonBuilder("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
-        ConcretePokemon squirtle = new PokemonBuilder("Squirtle", new ConcreteType("Water"), new Stats(10,48,65,43));
+        ConcretePokemon pikachu = new ConcretePokemon("Pikachu", new ConcreteType("Electric"), new Stats(35,55,40,90));
+        ConcretePokemon squirtle = new ConcretePokemon("Squirtle", new ConcreteType("Water"), new Stats(10,48,65,43));
         assertEquals("Pikachu", Fight.Winner(pikachu, squirtle));
 
     }
     @Test
     void metrics() {
         long start = System.nanoTime();
-        //winner();
+        winner();
         long finish = System.nanoTime();
         long timeElapsed = finish - start;
-        System.out.print(timeElapsed);
-        System.out.print("HELLLLLLLO");
+        System.out.println(timeElapsed);
+        System.out.println("nanoseconde");
 
     }
 }
